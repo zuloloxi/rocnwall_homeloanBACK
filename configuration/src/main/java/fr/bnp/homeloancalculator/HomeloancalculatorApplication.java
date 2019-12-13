7ebc901249363c5957be75a6f3930f3cfef522c9
rocnwall <christianbeard.bnp@gmail.com>
2019-12-13 14:23:57 +0100

    Commit initial - création de la structure du projet avec modules

---
+++ configuration/src/main/java/fr/bnp/homeloancalculator/HomeloancalculatorApplication.java
@@ -0,0 +1,13 @@
+package fr.bnp.homeloancalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeloancalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeloancalculatorApplication.class, args);
+	}
+
+}
