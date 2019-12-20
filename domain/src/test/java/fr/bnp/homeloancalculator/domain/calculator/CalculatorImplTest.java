539f6ccdea48990c43061b914c7212e877808432
rocnwall <christianbeard.bnp@gmail.com>
2019-12-20 17:02:37 +0100

    Ajout de nouvelles classes de test

--- domain/src/test/java/fr/bnp/homeloancalculator/domain/calculator/CalculatorImplTest.java
+++ domain/src/test/java/fr/bnp/homeloancalculator/domain/calculator/CalculatorImplTest.java
@@ -12,19 +12,50 @@ class CalculatorImplTest {
 
     @Test
     void should_return_xxxx_when_calculateLoanAmount() {
-        Assertions.assertEquals(12002.00,
-                calculator.calculateLoanAmount(
-                        Periodicity.MONTHLY,
-                        1.20,
-                        2629.00,
-                        15));
+        double result = calculator.calculateLoanAmount(
+                Periodicity.MONTHLY,
+                1.00,
+                1244.547635,
+                15);
+        /* Arrondir à deux décimales */
+        double arrondi = (double)Math.round(result * 100)/100;
+
+        Assertions.assertEquals(207946.37,arrondi);
     }
 
     @Test
-    void calculateLoanPayment() {
+    void should_return_xxxx_when_calculateLoanPayment() {
+        double result = calculator.calculateLoanPayment(
+                Periodicity.MONTHLY,
+                1.00,
+                300000.00,
+                15);
+
+        double arrondi = (double)Math.round(result * 100)/100;
+        Assertions.assertEquals(1795.48,arrondi);
     }
 
     @Test
-    void calculateCostOverview() {
+    void should_return_182_when_calculateCostOverview () {
+        double result = calculator.calculateCostOverview(
+                Periodicity.MONTHLY,
+                300000.00,
+                1795.483544,
+                15);
+
+        double arrondi = (double)Math.round(result * 100)/100;
+        Assertions.assertEquals(1.82,arrondi);
+    }
+
+    @Test
+    void should_return_186_when_calculateCostOverview () {
+        double result = calculator.calculateCostOverview(
+                Periodicity.MONTHLY,
+                200000.00,
+                1214.66,
+                15);
+
+        double arrondi = (double)Math.round(result * 100)/100;
+        Assertions.assertEquals(1.86,arrondi);
     }
 }
---
+++ domain/src/test/java/fr/bnp/homeloancalculator/domain/calculator/EIRCalculatorTest.java
@@ -0,0 +1,12 @@
+package fr.bnp.homeloancalculator.domain.calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EIRCalculatorTest {
+
+    @Test
+    void calculateEIR() {
+    }
+}
